695b0e2b32a10877f974575788c4f992b0a79806
Ignacio <ignacio.fernandez-grana@pasqal.com>
2024-04-01 15:54:19 +0200

    linting

--- qadence_libs/qinfo_tools/__init__.py
+++ qadence_libs/qinfo_tools/__init__.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from .qfi import get_quantum_fisher, get_quantum_fisher_spsa
 from .qng import QNG, QNG_SPSA
 
-
 # Modules to be automatically added to the qadence namespace
 __all__ = [
     "QNG",
