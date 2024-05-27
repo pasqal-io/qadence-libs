29cf30d8375aa5ee1c86f0489d0a904f5a84b46d
Ignacio <ignacio.fernandez-grana@pasqal.com>
2024-05-27 19:20:52 +0200

    reset vparams in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,9 @@
 # functions common to every test
 from __future__ import annotations
 
+import torch
 from pytest import fixture
-from qadence import QNN, BasisSet, QuantumCircuit
+from qadence import QNN, BasisSet, FeatureParameter, QuantumCircuit
 from qadence.constructors import feature_map, hamiltonian_factory, hea
 from qadence.operations import RX, RY, Z
 
@@ -11,7 +12,8 @@ from qadence.operations import RX, RY, Z
 @fixture
 def textbook_qfi_model() -> QNN:
     n_qubits, n_layers = [2, 2]
-    fm = feature_map(n_qubits, range(n_qubits), param="phi", fm_type=BasisSet.FOURIER)
+    feature_param = FeatureParameter("phi", value=0)
+    fm = feature_map(n_qubits, range(n_qubits), param=feature_param, fm_type=BasisSet.FOURIER)
     ansatz = hea(
         n_qubits,
         n_layers,
@@ -21,7 +23,9 @@ def textbook_qfi_model() -> QNN:
     )
     circuit = QuantumCircuit(n_qubits, ansatz, fm)
     obs = hamiltonian_factory(n_qubits, detuning=Z)
-    return QNN(circuit, [obs])
+    model = QNN(circuit, [obs])
+    model.reset_vparams(torch.zeros(model.num_vparams))
+    return model
 
 
 @fixture
